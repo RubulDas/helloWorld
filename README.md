71b7bf24fb04f801a910e416d6dbe4e4100e5212
lieugogoi <rgogoi@zaloni.com>
2014-10-13 16:32:47 +0530

    test git repo

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-Test
-====
-
-Lieu
+This is just to try and get hands of git project
---
+++ src/main/java/com/test/hadoop/sample/BasicWordCount.java
@@ -0,0 +1,59 @@
+package com.test.hadoop.sample;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * 
+ * @author lieu
+ * 
+ */
+public class BasicWordCount {
+	/**
+	 * 
+	 * @author
+	 * 
+	 */
+	public static class TokenizerMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable ONE= new IntWritable(1);
+		private final Text WORD = new Text();
+
+		public void map(final Object key, final Text value,
+				final Context context) throws IOException, InterruptedException {
+			final StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				WORD.set(itr.nextToken());
+				context.write(WORD, ONE);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @author lieu
+	 * 
+	 */
+	public static class IntSumReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+
+		private final IntWritable RESULT = new IntWritable();
+
+		public void reduce(final Text key, final Iterable<IntWritable> values,
+				final Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			RESULT.set(sum);
+			context.write(key, RESULT);
+		}
+	}
+
+}
