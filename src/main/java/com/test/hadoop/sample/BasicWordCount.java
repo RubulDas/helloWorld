c7720fc5777bc53bd250ffa26b6368f5a762efb0
Lieu <lieuranjan007@gmail.com>
2014-10-14 14:49:07 +0530

    Update BasicWordCount.java
    removing data

--- src/main/java/com/test/hadoop/sample/BasicWordCount.java
+++ src/main/java/com/test/hadoop/sample/BasicWordCount.java
@@ -8,11 +8,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
-/**
- * 
- * @author lieu
- * 
- */
 public class BasicWordCount {
 
 	public static class TokenizerMapper extends
